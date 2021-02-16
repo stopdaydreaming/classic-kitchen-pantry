aace25ac85d59ef008a3d76822b8c927b1bd6090
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-16 01:03:27 -0500

    fixed errors

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,86 +1,85 @@
 import React, { Component } from "react";
 import ItemRow from "../ItemRow/ItemRow";
-// import ItemForm from "../ItemForm/ItemForm"
 
 class Items extends Component {
   state = {
     items: [
       {
         id: 1,
-        name: "garlic salt",
-        calorieCount: 0
+        name: "Garlic Salt",
+        calorieCount: 0,
       },
       {
         id: 2,
-        name: "macaroni noodles",
-        calorieCount: 100
+        name: "Macaroni",
+        caloreCount: 100,
       },
       {
         id: 3,
-        name: "canned pinto beans",
-        calorieCount: 25
-      }
+        name: "Canned Pinto Beans",
+        calorieCount: 25,
+      },
     ],
-    newItem: "banana"
+    newItem: "",
   };
-
-  handleChange = (e) => {
+  
+  handleInputChange = (e) => {
     const { name, value } = e.target;
+    
     this.setState({
       [name]: value,
     });
   };
-
+  
   handleSubmit = (e) => {
     e.preventDefault();
-
+    
     const newItem = {
       id: this.state.items.length + 1,
-      name: this.state.newItem
+      name: this.state.newItem,
     };
-
+    
     const itemsArray = [...this.state.items];
     itemsArray.push(newItem);
-
+    
     this.setState({
       items: itemsArray,
+      newItem: ""
     });
   };
-
+  
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-12">
-            <table className="table mb-5">
+          <div className="col">
+            <table className="table">
               <thead>
                 <tr>
                   <th scope="col">#</th>
                   <th scope="col">Name</th>
-                  <th scope="col">Calorie Count</th>
                 </tr>
               </thead>
               <tbody>
-                {this.state.items.map(item => (
-                  <ItemRow key={item.id} {...item} />
+                {this.state.items.map((item) => (
+                  <ItemRow {...item} key={item.id} />
                 ))}
               </tbody>
             </table>
-            {/* <ItemForm value={this.state.newItem}/> */}
           </div>
         </div>
-
         <div className="row">
-          <div className="col-7">
+          <div className="col">
             <form onSubmit={this.handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="item">Add a new item to your pantry</label>
+              <div className="mb-3">
+                <label htmlFor="item" className="form-label">
+                  Add a new item to your pantry.
+                </label>
                 <input
                   type="text"
                   className="form-control"
                   id="item"
-                  defaultValue={this.state.newItem}
-                  //name must match key
+                  value={this.state.newItem}
                   name="newItem"
                   onChange={this.handleInputChange}
                 />
